8e51a3fc6b84dd3f7f7e9ba8f6e5d8a1398ddf5c
JacobPritchard <ubuntu@ip-172-31-3-208.ec2.internal>
2020-04-23 19:47:56 +0000

    Updated regs

--- REGS/src/drop.php
+++ REGS/src/drop.php
@@ -11,10 +11,13 @@ session_start();
     $cno =  $_GET['cno'];
     $dept = $_GET['dept'];
 
+    $drop1 = "DELETE FROM student_transcript WHERE t_id=$uid and cno=$cno and dept='$dept'";
+    mysqli_query($dbc, $drop1);
+
     $drop = "DELETE FROM courses_taken WHERE u_id=$uid and crn=$crn";
     mysqli_query($dbc, $drop);
 
-    echo $drop;
+    //echo $drop1;
     // die(mysqli_error($dbc));
 
     header("Location: course.php?cno=$cno&dept=$dept");
