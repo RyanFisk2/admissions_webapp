cf68e126588db6948b8ad7729d170473b018a00b
LSchiller1 <lschiller@gwu.edu>
2020-04-26 19:49:05 -0400

    Acceptance moves applicant to student table

--- APPS/reviewForms/submitDecision.php
+++ APPS/reviewForms/submitDecision.php
@@ -24,6 +24,12 @@
 					//delete application from DB once decision is made
 					$updateQuery = "UPDATE application_form SET decision = $final WHERE userID = '$applicantID'";
 
+					if(($final == 2) || ($final == 3)){
+						//applicant has been admitted, move them to students table TODO:make the applicant accept first
+						$updateStatusQuery = "UPDATE users SET p_level = '5' WHERE id = '$applicantID'";
+						$statusResult = mysqli_query($dbc, $updateStatusQuery);
+					};
+
 					if($updateResult = mysqli_query($dbc, $updateQuery)){
 						$msg = "Your application has been reviewed! Login to view your status!";
                                         	header("Location: ../index.php");
